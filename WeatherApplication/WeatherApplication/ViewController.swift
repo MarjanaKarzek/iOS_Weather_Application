8b82f5180628e5d3ee6f04d0d4ce1a5b6888c39f
MarjanaKarzek <marjana.karzek@outlook.com>
2017-08-02 16:17:10 +0200

    [#5] feat (*) -> add project folder and app layout

---
+++ WeatherApplication/WeatherApplication/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherApplication
+//
+//  Created by Marjana Karzek on 02/08/17.
+//  Copyright © 2017 Marjana Karzek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
