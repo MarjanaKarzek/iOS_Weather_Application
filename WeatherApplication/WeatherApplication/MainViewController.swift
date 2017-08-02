6b1ef58e0eb28d12369612eed922d545eb0f60c1
MarjanaKarzek <marjana.karzek@outlook.com>
2017-08-02 16:53:00 +0200

    [#10] feat (MainViewController) -> add sharing functionallity for facebook and twitter

--- WeatherApplication/WeatherApplication/MainViewController.swift
+++ WeatherApplication/WeatherApplication/MainViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Social
+
+private var testimages = ["testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg","testimage.jpg"]
 
 class MainViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,4 +35,21 @@ class MainViewController: UIViewController {
     }
     */
 
+    @IBAction func facbookButtonPushed(sender: UIButton){
+        let facebookSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        facebookSheet.setInitialText("This is the weather for the next 10 days")
+        for imagetitle in testimages{
+            facebookSheet.add(UIImage(named: imagetitle))
+        }
+        self.present(facebookSheet, animated:true, completion:nil)
+    }
+    
+    @IBAction func twitterButtonPushed(sender: UIButton) {
+        let twitterSheet:SLComposeViewController = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        twitterSheet.setInitialText("This is the weather for the next 10 days")
+        for imagetitle in testimages{
+            twitterSheet.add(UIImage(named: imagetitle))
+        }
+        self.present(twitterSheet, animated: true, completion: nil)
+    }
 }
